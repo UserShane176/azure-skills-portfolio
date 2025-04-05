0e10ea83c02bd1489318972bec8613ed94c0a9a9
ShaneHung <poxuan33@gmail.com>
2025-04-05 11:22:21 +0800

    Added 10 new LeetCode SQL solutions, including problems on aggregation, joins, and subqueries

--- SQL_LeetCode_SQL/620_not_boring_movies.sql
+++ SQL_LeetCode_SQL/620_not_boring_movies.sql
@@ -1,4 +1,4 @@
 SELECT id, movie, description, rating
 FROM Cinema
-WHERE id % 2 != 0 AND description != 'boring'
-ORDER BY id DESC 
+WHERE id % 2 = 1 AND description != 'boring'
+ORDER BY rating DESC 
